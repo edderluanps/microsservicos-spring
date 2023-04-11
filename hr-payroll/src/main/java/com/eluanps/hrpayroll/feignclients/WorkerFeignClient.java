fcc194c1dfaf2eaf9b9c5540ba5caf19c4d957e0
Edder Luan <eluann100@gmail.com>
2023-04-11 17:43:28 -0300

    Feign Client

---
+++ hr-payroll/src/main/java/com/eluanps/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package com.eluanps.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.eluanps.hrpayroll.entity.Worker;
+
+@Component
+@FeignClient(name = "hrworker", url = "localhost:8001", path = "/ms/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/com/eluanps/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/eluanps/hrpayroll/services/PaymentService.java
@@ -2,28 +2,21 @@ package com.eluanps.hrpayroll.services;
 
 import com.eluanps.hrpayroll.entity.Payment;
 import com.eluanps.hrpayroll.entity.Worker;
-import java.util.HashMap;
-import java.util.Map;
+import com.eluanps.hrpayroll.feignclients.WorkerFeignClient;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
-    @Value("${hrworker.host}")
-    private String workerHost;
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
 
-    @Autowired
-    private RestTemplate restTemplate;
+	public Payment getPayment(long workerId, int days) {
 
-    public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerId);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
 
-        Worker worker = restTemplate.getForObject(
-                workerHost + "/ms/workers/{id}", Worker.class, uriVariables);
-        return new Payment(worker.getName(), worker.getDailyIncome(), days);
-    }
 }
