fb2170dc5e5a213ca95562b4bb6c68bc9b21a08a
Edder Luan <eluann100@gmail.com>
2023-04-11 17:13:43 -0300

    Rest template

--- hr-payroll/src/main/java/com/eluanps/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/eluanps/hrpayroll/services/PaymentService.java
@@ -1,12 +1,29 @@
 package com.eluanps.hrpayroll.services;
 
 import com.eluanps.hrpayroll.entity.Payment;
+import com.eluanps.hrpayroll.entity.Worker;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
+    @Value("${hrworker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workerId);
+
+        Worker worker = restTemplate.getForObject(
+                workerHost + "/ms/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
