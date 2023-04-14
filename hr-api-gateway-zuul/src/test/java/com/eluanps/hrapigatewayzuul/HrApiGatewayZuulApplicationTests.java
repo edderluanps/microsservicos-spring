6d9440add1cf33de7c7b02ed0a078ba2f93aa989
Edder Luan <eluann100@gmail.com>
2023-04-13 21:41:05 -0300

    Implementando o Zuul API Gateway

---
+++ hr-api-gateway-zuul/src/test/java/com/eluanps/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluanps.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
