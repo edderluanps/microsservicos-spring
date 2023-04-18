b31315f6921ad6713201ffd6cfaf2c33de1c074c
Edder Luan <eluann100@gmail.com>
2023-04-17 23:01:35 -0300

    Hr-Worker Server client

--- hr-worker/src/main/java/com/eluanps/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/eluanps/hrworker/resources/WorkerResource.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,12 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 
 	@Autowired
 	private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
+    
+    @GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
