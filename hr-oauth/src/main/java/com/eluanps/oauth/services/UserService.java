7bd22b6518abecc0f2e04803cbe7945286422fd0
Edder Luan <eluann100@gmail.com>
2023-04-20 17:52:58 -0300

    Login com Token JWT

--- hr-oauth/src/main/java/com/eluanps/oauth/services/UserService.java
+++ hr-oauth/src/main/java/com/eluanps/oauth/services/UserService.java
@@ -3,14 +3,17 @@ package com.eluanps.oauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.eluanps.oauth.entity.User;
 import com.eluanps.oauth.feignclients.UserFeignClient;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -26,4 +29,16 @@ public class UserService {
 		logger.info("Email found: " + email);
 		return user;
 	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
+	}
+	
 }
