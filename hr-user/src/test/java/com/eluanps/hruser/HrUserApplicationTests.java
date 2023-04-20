d4741ec1cc5c2401f2e2653504828952ccfc7b09
Edder Luan <eluann100@gmail.com>
2023-04-20 15:19:51 -0300

    Implementando o Serviço Hr-User

---
+++ hr-user/src/test/java/com/eluanps/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluanps.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
